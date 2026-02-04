844de4c4315cba5d36f4591866b2936ae3c493f1
Akhil <96526208+Akhil-peram@users.noreply.github.com>
2026-02-04 23:20:36 +0530

    Add files via upload

---
+++ hello.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+
+func main(){
+fmt.Println("Hello Go")
+var a int = 10
+var b = 20
+c:=30
+fmt.Println(a,b,c)
+}
---
+++ info.go
@@ -0,0 +1,13 @@
+package main 
+
+import "fmt"
+
+func main(){
+
+name:="Akhil"
+age:=25
+city:="Nellore"
+fmt.Println("Name:",name)
+fmt.Println("Age:",age)
+fmt.Println("City",city)
+}

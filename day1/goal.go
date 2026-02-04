915fc84a4b57bc8ae9208a605c541d2f2d6349b4
Akhil <96526208+Akhil-peram@users.noreply.github.com>
2026-02-04 23:23:30 +0530

    Add files via upload

---
+++ day1/goal.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+name:="Akhil"
+city:="Nellore"
+goal:="To get 5 Crore package in MNC"
+
+
+fmt.Println("Name:",name)
+fmt.Println("City:",city)
+fmt.Println("Goal:",goal)
+}
---
+++ day1/sum.go
@@ -0,0 +1,15 @@
+package main 
+
+import "fmt"
+
+
+func main(){
+
+a:=20
+b:=30
+
+fmt.Println("A+B=",a+b)
+
+
+
+}

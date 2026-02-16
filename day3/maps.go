7d72f42d8d67edb85fd3ffeaaa227341230bf487
Akhil <96526208+Akhil-peram@users.noreply.github.com>
2026-02-16 22:31:00 +0530

    Update maps.go

--- day3/maps.go
+++ day3/maps.go
@@ -4,8 +4,9 @@ import "fmt"
 
 func main(){
 
-details := map["string"]int{"A":1,"B":11,"C":2}
+details := map[string]int{"A":1,"B":11,"C":2}
 
   fmt.Println(details)
-  
+themes := map[string]string{"Batman":"I am the knight","ironman":"i am ironman",}
+  fmt.Println(themes)
 }
